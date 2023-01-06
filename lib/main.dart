d1e4455a400172beaa0a4abe2a35575fd7785be1
mohmed <mohamedahmed200111232425@gmail.com>
2023-01-06 17:40:48 +0200

    Receive data from the API and make a screenshot of the category, news, tab controller and drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/layout/home_layout.dart';
+import 'package:news_c7_fri/layout/home_layout.dart';
 
 void main() {
   runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     initialRoute:HomeLayout.routName,
-     routes: {
-       HomeLayout.routName:(context) => HomeLayout(),
-
-     },
-     debugShowCheckedModeBanner: false,
-
-   );
+    return MaterialApp(
+      initialRoute: HomeLayout.routeName,
+      routes:{
+        HomeLayout.routeName:(context) => HomeLayout()
+      } ,
+      debugShowCheckedModeBanner: false,
+    );
   }
-
 }
+
